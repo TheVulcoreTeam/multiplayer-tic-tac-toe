40ccb1c8c92b260316a4a545dfc8c1414a8c7e5d
Matías Valentín Muñoz Espinoza <writkas2@gmail.com>
2021-08-22 14:04:22 -0400

    Estructura de directorios

---
+++ Autoloads/Network.gd
@@ -0,0 +1,51 @@
+extends Node
+
+const DEFAULT_PORT = 9080
+const MAX_CLIENTS = 4
+
+var server
+var client
+
+var ip_address := "0.0.0.0"
+
+func _ready() -> void:
+	get_tree().connect("connected_to_server", self, "_connected_to_server")
+	get_tree().connect("server_disconnected", self, "_server_disconnected")
+
+
+func create_server() -> void:
+	server = WebSocketServer.new()
+	var result = server.listen(DEFAULT_PORT, PoolStringArray(), true)
+	
+	if result == OK:
+		get_tree().network_peer = server
+		server.connect("peer_connected", self, "player_connected")
+		server.connect("peer_disconnected", self, "player_disconnected")
+
+
+func join_server() -> void:
+#	client = NetworkedMultiplayerENet.new()
+	client = WebSocketClient.new()
+	var result = client.connect_to_url(ip_address, PoolStringArray(), true)
+	
+	if result == OK:
+		get_tree().network_peer = client
+		client.connect("peer_connected", self, "player_connected")
+		client.connect("peer_disconnected", self, "player_disconnected")
+
+
+func _connected_to_server() -> void:
+	print("Se ha conectado al Server exitosamente.")
+
+
+func _server_disconnected() -> void:
+	print("Se ha desconectado del Server.")
+
+
+func player_connected():
+	print_debug("connected")
+
+
+func player_disconnected():
+	print_debug("disconnected")
+
---
+++ Network/NetworkSetup.gd
@@ -0,0 +1,60 @@
+extends Control
+
+onready var multiplayer_config_ui = $Multiplayer_configure
+onready var server_ip_address = $Multiplayer_configure/VBoxLabels/ServerIPAdress
+onready var device_ip_address = $CanvasLayer/device_ip_address
+
+var move_up = Vector2.ZERO
+
+
+func _process(delta):
+	if Network.server!=null:
+		if Network.server.is_listening(): # is_listening is true when the server is active and listening
+			Network.server.poll()
+	elif Network.client!=null:
+		if (Network.client.get_connection_status() == NetworkedMultiplayerPeer.CONNECTION_CONNECTED or
+			Network.client.get_connection_status() == NetworkedMultiplayerPeer.CONNECTION_CONNECTING):
+				Network.client.poll()
+
+func _ready() -> void:
+	get_tree().connect("network_peer_connected", self, "_player_connected")
+	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
+	get_tree().connect("connected_to_server", self, "_connected_to_server")
+	
+#
+#	device_ip_address.text = Network.ip_address
+	
+	if OS.has_feature("Server"):
+		Network.create_server()
+
+
+func _player_connected(id) -> void:
+	print_debug("Player " + str(id) + " has connected")
+
+
+func _player_disconnected(id) -> void:
+	print_debug("Player " + str(id) + " has disconnected")
+
+
+func _on_create_server_pressed():
+	multiplayer_config_ui.hide()
+	Network.create_server()
+
+
+func _on_join_server_pressed():
+	if server_ip_address.text != "":
+		multiplayer_config_ui.hide()
+		Network.ip_address = server_ip_address.text
+		Network.join_server()
+
+
+func _input(event):
+	if event.is_action_pressed("ui_up"):
+		$Test.text = $Test.text + "input() "
+		rpc("move")
+
+
+remotesync func move():
+	$Test.text = $Test.text + "move() "
+	$Player1.global_position += Vector2.UP * 10
+
